9ee132e5b2c94ee3164f3468e36366d5127dfd25
Tschan <sebastian.tschan@training.lug-ag.de>
2024-12-02 10:18:01 +0100

    BubbleOne

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] liste = { 4, 33, 7, 1, 23, 12 };  // ohne sort.methode
+
+                    // i = elementIndex, j = sortierSchleife
+
+            for (int i = 0; i < liste.Length - 1; i++)     //Schleife bis alle Zahlen sortiert sind
+            { 
+                for (int j = 0;j < liste.Length - i - 1; j++)  // SChleife 2 innerhalb Schleife 1
+                {
+                    if (liste[j] > liste[j + 1])
+                    {
+                        int platzhalter = liste[j];    //Tauscht Platzhalter mit Listenplatz
+                        liste[j] = liste[j + 1];
+                        liste[j + 1] = platzhalter;
+
+                        
+                    }
+                }
+             }
+            foreach (int k in liste)
+            { 
+                Console.WriteLine(k);
+            }
+
+            Console.WriteLine(string.Join(" , ", liste));
+        }
+    }
+}
